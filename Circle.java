d6d2f8a7d36f308a59b1a3aa7939caf8579cfc23
SobertCrane <philip03phlip03@gmail.com>
2022-06-13 14:47:02 +0800

    javatest

---
+++ Circle.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Circle {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("Please enter your radius ");
+		String radius = in.readLine();
+		
+		double pi = 3.1415926;
+		System.out.println("Area = "+Integer.parseInt(radius)*Integer.parseInt(radius)*pi);
+		System.out.println("Circumfrerence ="+ Integer.parseInt(radius)*2*pi);
+	}
+}
