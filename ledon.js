e943cee6fc90e2cebadd14e29caafbab8ac4bccf
azenlu56 <107386321+azenlu56@users.noreply.github.com>
2022-06-13 19:10:21 +0800

    Add files via upload
    azen commit test

--- ledon.js
+++ ledon.js
@@ -1,5 +1,5 @@
 var Gpio = require("onoff").Gpio,
     led = new Gpio(17, "out");
-    led.write(1, function(){
-        console.log("LED on");
+    led.write(0, function(){
+        console.log("LED off");
     });
